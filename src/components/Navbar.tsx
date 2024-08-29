e0acc5cf978cd827f3b88303fa7e4f587e7b984c
pallavi-sahu <pallavisahu0610@gmail.com>
2024-08-29 20:52:22 +0530

    project initializing

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Typography>SAHU Welding & Engineering Workshop</Typography>
+    </div>
+  )
+}
+
+export default Navbar
