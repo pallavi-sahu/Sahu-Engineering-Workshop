322789015eb895a495b3ada04425f2fa88e2cd31
pallavi-sahu <pallavisahu0610@gmail.com>
2024-09-01 19:25:32 +0530

    included footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-    {/* <Navbar/> */}
+    <Navbar/>
     <HomePage/>
+    <Footer/>
     </>
   );  
 }
