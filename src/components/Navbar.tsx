021abeb543ac77b314b31ea118209367f319f707
pallavi-sahu <pallavisahu0610@gmail.com>
2024-09-01 12:26:51 +0530

    edited navbar accordian

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import Typography from '@mui/material/Typography';
+import Accordion from '@mui/material/Accordion';
 
+const carousel = [
+
+]
 const Navbar = () => {
-  return (
-    <div>
-      <Typography>SAHU Welding & Engineering Workshop</Typography>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+            <Typography variant="h5">
+                SAHU Welding and Engineering Workshop
+            </Typography>
+            </div>
+            <div>
+                <Typography variant='body2'>Home</Typography>
+                <Typography variant='body2'>About Us</Typography>
+                <Typography variant='body2'>Contact</Typography>
+            </div>
+            <div>
+                {/* <Accordion children={'About Us'}/> */}
+            </div>
+
+        </div>
+    )
 }
 
 export default Navbar
