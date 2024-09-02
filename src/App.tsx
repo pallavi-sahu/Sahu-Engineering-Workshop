293a5c3ab1b927c395689905a34cc53d9ed39047
pallavi-sahu <pallavisahu0610@gmail.com>
2024-09-03 00:32:31 +0530

    added services component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <>
     <Navbar/>
     <HomePage/>
+    <Contact/>
     <Footer/>
     </>
   );  
