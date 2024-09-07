2a8bacac05145ceeb2e6725e3286832fe40a32a2
pallavi-sahu <pallavisahu0610@gmail.com>
2024-09-07 23:58:11 +0530

    Added category component to the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <>
     <Navbar/>
     <HomePage/>
+    <Categories/>
     <Contact/>
     <Footer/>
     </>
