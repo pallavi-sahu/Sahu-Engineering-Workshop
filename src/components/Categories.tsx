076c62ac1e3e824f4e5accee8fd2c77e6a762e42
pallavi-sahu <pallavisahu0610@gmail.com>
2024-09-08 00:33:45 +0530

    Added some css on catogories cards

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import steel from '../assets/images/steel.jpeg';
 import iron from '../assets/images/welding.jpg';
 import tractor from '../assets/images/tractor-trolley-1.jpg';
+// import { useNavigate } from 'react-router-dom';
 type category = {
     id: number,
     image: string
@@ -21,23 +22,27 @@ const categories: category[] = [
     }
 ]
 const Categories = () => {
+    // const navigate = useNavigate()
     return (
-        <div className=''>
-            <div>
-                {
-                    categories.map((cat) => {
-                        return (
-                            <div className='border-primary-orange rounded-md'>
-                                <p>Category {cat.id}</p>
-                                <div className='rounded-md'>
-                                    <img src={cat.image} alt={`catogory ${cat.id}`}/>
-                                </div>
-
+        <div className='flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-4 border-b-2 border-bgrey p-10'>
+            {
+                categories.map((cat) => {
+                    return (
+                        <div className='border-2 border-primary-orange rounded-md p-4 flex flex-col gap-5 items-center justify-between'>
+                            <p className='text-center md:text-left'>Category {cat.id}</p>
+                            <div className='rounded-md flex justify-center items-center '>
+                                <img src={cat.image} alt={`catogory ${cat.id}`} className="h-32 w-64 object-cover"/>
                             </div>
-                        )
-                    })
-                }
-            </div>
+                            <button
+                                className="mt-4 md:mt-0 bg-primary-orange text-white py-2 px-4 rounded-md"
+                                // onClick={() => navigate(`/category/${cat.id}`)}
+                                 >
+                                    View more
+                                </button>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
