40204066f007bf75a1c24848ec9d1770cd1879cb
pallavi-sahu <pallavisahu0610@gmail.com>
2024-09-09 22:30:47 +0530

    added a new color for border orange

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
       colors: {
         'primary-blue': '#1F499E',
         'primary-orange': '#F59121',
-         bgrey : '#D9D9D9'
+         bgrey : '#D9D9D9',
+         'border-orange':'#FFCE85'
       },
       fontFamily:{
         sans: ['Inter', 'ui-sans-serif', 'system-ui'],
