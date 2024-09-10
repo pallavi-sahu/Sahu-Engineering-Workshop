683aad6d8ee4ed2770dfca179ec050916d4eb52a
pallavi-sahu <pallavisahu0610@gmail.com>
2024-09-10 23:27:05 +0530

    Created new component to show individual category related details and images

---
+++ src/components/IndividualCategory.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const IndividualCategory = () => {
+  return (
+    <div>
+        <div>Category</div>
+        <div>Description</div>
+        <div>Images</div>
+    </div>
+  )
+}
+
+export default IndividualCategory
