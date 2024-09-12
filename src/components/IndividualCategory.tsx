0e531f8c9cf76d447f3e0cecd6c37e72fff094ca
pallavi-sahu <pallavisahu0610@gmail.com>
2024-09-12 23:05:49 +0530

    Individual component completed in statitically further i will make it dynamic

--- src/components/IndividualCategory.tsx
+++ src/components/IndividualCategory.tsx
@@ -15,27 +15,33 @@ const products = [
     img:"",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
+  {
+    id: 3,
+    heading: "Product 3",
+    img:"",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
 ]
 const IndividualCategory = () => {
   return (
-    <div>
-      <div className='flex flex-row gap-10 p-4'>
-      <div>Category 1</div>
+    <div className='md:pl-8 md:pr-8'>
+      <div className='flex flex-row gap-5 md: gap:10 border-b-2 p-4'>
+      <div className='border-b-4 border-black '>Category 1</div>
       <div>Category 2</div>
       <div>Category 3</div>
       </div>
 
-      <div className='bg-red w-full border-1 p-4 flex justify-center items-center'>Category</div>
+      <div className='m-4 border-2 text-center p-2 bg-grey font-bold text-xl text-primary-blue rounded-md'>Category - 1</div>
       {
         products.map((product) => {
           return (
-            <div>
-              <div>
-                <p>{product.heading}</p>
+            <div className='p-6 flex flex-col md:flex-row gap-4 border-2 m-4 rounded-md'>
+              <div className='flex flex-col'>
+                <p className='text-xl font-bold'>{product.heading}</p>
                 <p>{product.description}</p>
               </div>
-              <div>
-                <img src={product.img} alt={product.heading}/>
+              <div className='w-full md:w-[33vw] rounded-md'>
+                <img src={product.img} alt={product.heading} className='rounded-md'/>
               </div>
             </div>
           )
