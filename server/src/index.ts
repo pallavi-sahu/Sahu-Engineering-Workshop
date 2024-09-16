0960de2dffaeef706dae537d91c3c66fd9838524
pallavi-sahu <pallavisahu0610@gmail.com>
2024-09-16 20:03:07 +0530

    updated packages and added typescript for backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+app.use(cors());
 
-app.get('/api/test', (req, res)=> {
+app.get('/api/test', (req, res:any)=> {
     res.json({message: "Hello from backend"})
 })
 
